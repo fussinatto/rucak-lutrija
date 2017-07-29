e5cb0e69ba76246ffa780eab80a21622eae11c84
Dario <fussinatto@gmail.com>
2017-07-29 20:57:51 +0200

    add env file

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,9 @@ const router = express.Router();
 // Do work here
 router.get('/', (req, res) => {
   res.send('Hey! It works!');
+  // res.send(req.query) // ?name=Duri&no=11
+  // res.send(req.params) /:something
 });
 
 module.exports = router;
+
